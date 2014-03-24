6c6fc1a16c57021ca3460bd16340e2ebca95d4e0
vamshis <vamshi@shippable.com>
2014-03-24 13:46:59 +0530

    removed commented lines..

--- sample.rb
+++ sample.rb
@@ -1,16 +1,10 @@
-puts "starting.."
-#require 'jruby'
-#require 'thrift/struct_union'
-#require 'thrift'
-#require 'rubygems'
-#require 'thrift_client'
+
 require 'cassandra'
-#include SimpleUUID
 
-puts "gems loaded.."
-
+puts "creating new keyspace.."
 client = Cassandra.new('shippableDemo', '127.0.0.1:9160')
 
+puts "inserting data.."
 client.insert("users", 'userone', {
     'full_name' => 'User One',
     'email' => 'uone@example.com'
@@ -21,10 +15,10 @@ client.insert("users", 'usertwo', {
     'email' => 'utwo@example.com'
 })
 
-puts "retrieving data.."
-
+puts "output data from the program.."
 client.each_key("users") do |user|
-   puts user
+   puts "key : " + user
 end
+
 
 
